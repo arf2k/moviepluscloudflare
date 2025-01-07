f1548c3bf35dae1a1761e07327c68f8b886c6b31
https://learn.co/arf2k <foreman.alexander@gmail.com>
2025-01-06 22:11:41 -0500

    Add nav for back button on movie details page

--- react-frontend/src/pages/MovieDetailPage.jsx
+++ react-frontend/src/pages/MovieDetailPage.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const baseWorkerUrl = import.meta.env.VITE_API_URL;
-
 export default function MovieDetailPage({ token }) {
+  const navigate = useNavigate();
   const { imdbID } = useParams();
   const [movie, setMovie] = useState(null);
   const [error, setError] = useState('');
@@ -30,15 +31,19 @@ export default function MovieDetailPage({ token }) {
   if (!movie) return <p>Loading...</p>;
 
   return (
-    <div className="movie-detail-page">
-      <h1>{movie.Title}</h1>
-      <img src={movie.Poster} alt={`${movie.Title} Poster`} />
-      <p>Released: {movie.Released}</p>
-      <p>Runtime: {movie.Runtime}</p>
-      <p>Genre: {movie.Genre}</p>
-      <p>Director: {movie.Director}</p>
-      <p>IMDB Rating: {movie.imdbRating}</p>
-      <p>Plot: {movie.Plot}</p>
-    </div>
+    <><div className="movie-detail-page">
+            <h1>{movie.Title}</h1>
+            <img src={movie.Poster} alt={`${movie.Title} Poster`} />
+            <p>Released: {movie.Released}</p>
+            <p>Runtime: {movie.Runtime}</p>
+            <p>Genre: {movie.Genre}</p>
+            <p>Director: {movie.Director}</p>
+            <p>IMDB Rating: {movie.imdbRating}</p>
+            <p>Plot: {movie.Plot}</p>
+       </div>
+       <div>
+       <button onClick={() => navigate(-1)}>Back</button>
+       </div>
+    </>
   );
 }
