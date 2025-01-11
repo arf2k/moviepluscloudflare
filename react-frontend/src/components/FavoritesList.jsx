ab5cbf4371fa9fb12410374a5f2268c9c5642e50
https://learn.co/arf2k <foreman.alexander@gmail.com>
2025-01-11 18:24:59 -0500

    Remove functionality works add favoriteslist to homepage

--- react-frontend/src/components/FavoritesList.jsx
+++ react-frontend/src/components/FavoritesList.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { useFavorites } from '../context/FavoritesContext';
 
-export default function FavoritesList({ token }) {
-  // You could fetch favorites from your Worker in useEffect
-  // or store them in React Context, etc.
+export default function FavoritesList() {
+  const { favorites } = useFavorites();
+
   return (
     <div id="favorites-section">
-      <h2>Favorites</h2>
-      <div id="favorites-list">
-        {/* Render your favorites here */}
+      <h2>Your Favorites</h2>
+      {favorites.length > 0 ? (
+        <div id="favorites-list">
+          {favorites.map((movie) => (
+            <div key={movie.movieId} className="favorite-item">
+              {movie.posterPath ? (
+                <img
+                  src={`https://image.tmdb.org/t/p/w200${movie.posterPath}`}
+                  alt={`${movie.title} Poster`}
+                />
+              ) : (
+                <div className="placeholder">No Image Available</div>
+              )}
+              <Link to={`/movie/${movie.movieId}`}>{movie.title}</Link>
+            </div>
+          ))}
+        </div>
+      ) : (
         <p>You have no favorites yet.</p>
-      </div>
+      )}
     </div>
   );
 }
