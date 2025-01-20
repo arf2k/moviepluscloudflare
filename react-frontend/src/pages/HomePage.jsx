6d0a6b9bc048dc3341a1daba76245237c2845a47
https://learn.co/arf2k <foreman.alexander@gmail.com>
2025-01-20 15:17:05 -0500

    Fix link functionality from react router on homepage

--- react-frontend/src/pages/HomePage.jsx
+++ react-frontend/src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useAuth } from '../context/AuthContext';
 import MovieSearch from '../components/MovieSearch';
 import FavoritesList from '../components/FavoritesList';
+import { Link } from 'react-router-dom';
 
 export default function HomePage() {
   const { logout } = useAuth();
